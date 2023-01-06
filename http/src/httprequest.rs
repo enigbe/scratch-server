24673a759785eedb6a7371b86f6f037dea456266
Enigbe Ochekliye <ochekliyeenigbe@gmail.com>
2023-01-06 08:38:59 +0100

    feat: httpresponse

--- http/src/httprequest.rs
+++ http/src/httprequest.rs
@@ -124,4 +124,20 @@ mod tests {
         let v: Version = "HTTP/1.1".into();
         assert_eq!(v, Version::V1_1);
     }
+
+    #[test]
+    fn test_read_http() {
+        let s = String::from("GET /greeting HTTP/1.1\r\nHost: localhost:3000\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n");
+        let mut headers_expected = HashMap::new();
+        headers_expected.insert("Host".into(), " localhost".into());
+        headers_expected.insert("Accept".into(), " */*".into());
+        headers_expected.insert("User-Agent".into(), " curl/7.64.1".into());
+
+        let req: HttpRequest = s.into();
+
+        assert_eq!(Method::Get, req.method);
+        assert_eq!(Version::V1_1, req.version);
+        assert_eq!(Resource::Path("/greeting".to_string()), req.resource);
+        assert_eq!(headers_expected, req.headers);
+    }
 }
